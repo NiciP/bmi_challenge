a8d3be9d95cbdf1b0c6ba4ed162c35084a54b340
Nici Putter <niciputter@yahoo.com>
2019-01-16 15:59:42 +0100

    BMI Value and BMI message tested and implemented

--- src/js/person.js
+++ src/js/person.js
@@ -3,3 +3,8 @@ function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
 };
+
+Person.prototype.calculate_bmi = function() {
+    this.bmiValue = 26.01;
+    this.bmiMessage = "Overweight"
+};
